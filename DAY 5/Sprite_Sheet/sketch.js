dff414139b740b60530324b3ba2b423dcf4dfc9e
thav-arch <thavanesh.n.official@gmail.com>
2025-10-13 11:33:37 +0530

    Update sketch.js

--- DAY 5/Sprite_Sheet/sketch.js
+++ DAY 5/Sprite_Sheet/sketch.js
@@ -36,50 +36,50 @@
 // }
 
 
-let spriteImg;
-let sRows = 4, sCols = 8;
-let sprites = [];
-let x, y; 
-let speed = 5;
-let frameIndex = 0;
-let frameDelay = 5;
-let frameCounter = 0;
+// let spriteImg;
+// let sRows = 4, sCols = 8;
+// let sprites = [];
+// let x, y; 
+// let speed = 5;
+// let frameIndex = 0;
+// let frameDelay = 5;
+// let frameCounter = 0;
 
-function preload() {
-  spriteImg = loadImage("Images/explosionFull.png");
-}
+// function preload() {
+//   spriteImg = loadImage("Images/explosionFull.png");
+// }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   imageMode(CENTER);
 
-  let sWidth = spriteImg.width / sCols;
-  let sHeight = spriteImg.height / sRows;
+//   let sWidth = spriteImg.width / sCols;
+//   let sHeight = spriteImg.height / sRows;
 
-  for (let i = 0; i < sRows; i++) {
-    for (let j = 0; j < sCols; j++) {
-      let frame = spriteImg.get(j * sWidth, i * sHeight, sWidth, sHeight);
-      sprites.push(frame);
-    }
-  }
+//   for (let i = 0; i < sRows; i++) {
+//     for (let j = 0; j < sCols; j++) {
+//       let frame = spriteImg.get(j * sWidth, i * sHeight, sWidth, sHeight);
+//       sprites.push(frame);
+//     }
+//   }
 
-  x = width / 2;
-  y = height / 2;
-}
+//   x = width / 2;
+//   y = height / 2;
+// }
 
-function draw() {
-  background(0);
+// function draw() {
+//   background(0);
 
-  if (keyIsDown(68)) x += speed; // D
-  if (keyIsDown(65)) x -= speed; // A
-  if (keyIsDown(87)) y -= speed; // W
-  if (keyIsDown(83)) y += speed; // S
+//   if (keyIsDown(68)) x += speed; // D
+//   if (keyIsDown(65)) x -= speed; // A
+//   if (keyIsDown(87)) y -= speed; // W
+//   if (keyIsDown(83)) y += speed; // S
 
-  frameCounter++;
-  if (frameCounter >= frameDelay) {
-    frameCounter = 0;
-    frameIndex = (frameIndex + 1) % sprites.length;
-  }
+//   frameCounter++;
+//   if (frameCounter >= frameDelay) {
+//     frameCounter = 0;
+//     frameIndex = (frameIndex + 1) % sprites.length;
+//   }
 
-  image(sprites[frameIndex], x, y, 64, 64);
-}
+//   image(sprites[frameIndex], x, y, 64, 64);
+// }
